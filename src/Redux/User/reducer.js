d54fa1da0de8e0bfd9d9ed2eb04776e12ec962c4
Ramdas Hedgapure <hedgapurer@gmail.com>
2022-03-15 10:30:41 +0530

    package fixed

--- src/Redux/User/reducer.js
+++ src/Redux/User/reducer.js
@@ -16,3 +16,4 @@ export const userReducer = (state = {}, {type, payload})=>{
                 return state;
     }
 }
+
--- src/stores/store.js
+++ src/stores/store.js
@@ -4,4 +4,4 @@ import {createStore, combineReducers} from 'redux'
 const rootReducer = combineReducers({
     userState : userReducer
 })
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__())
+export const store = createStore(rootReducer)
